bed86c0c8c0e8d1cc3950a8a782820e5d80967e6
AliceChpmnk <alisablog50@gmail.com>
2023-03-05 10:29:34 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,34 @@ import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import { StyledForm } from './Form/Form.styled';
 
-class App extends Component {
-  state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+const testContacts = [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]
+
+class App extends Component {
+  state = {
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    }
+
+    console.log(`Contacts to test: `);
+    console.log(testContacts);
+  }
+
+  componentDidUpdate(_, prevState) {
+    const prevcontacts = prevState.contacts;
+    const nextContacts = this.state.contacts;
+    if(prevcontacts !== nextContacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
   }
 
   isInContacts = (name) => {
